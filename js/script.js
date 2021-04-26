3002206041aad1fc838eadbe813a4288fc60536d
Mary Phelan <C00249325@itcarlow.ie>
2021-04-26 13:41:52 +0100

    recent files

--- js/script.js
+++ js/script.js
@@ -1,10 +1,14 @@
 // Each time this function is called a GameObject
 // is create based on the arguments
+// In JavaScript you can consider everything an Object
+// including functions
+
 function GameObject(name, img, health) {
     this.name = name;
     this.img = img;
     this.health = health;
 }
+
 
 // The GamerInput is an Object that holds the Current
 // GamerInput (Left, Right, Up, Down)
@@ -12,26 +16,32 @@ function GamerInput(input) {
     this.action = input;
 }
 
+// log the URL the page request came from
+function onPageLoad() {
+
+    var href = window.location.href;
+    alert(protocol);
+
+    var protocol = window.location.protocol;
+    alert(protocol);
+
+    var host = window.location.host;
+    alert(host);
+
+
+    var pathname = window.location.pathname;
+    alert(pathname);
+
+    var search = window.location.search;
+    alert(search);
+
+}
 
 // get a handle to the canvas context
 var canvas = document.getElementById("the_canvas");
 
 // get 2D context for this canvas
 var context = canvas.getContext("2d");
-
-// Update Heads Up Display with Weapon Information
-function weaponSelection() {
-    var selection = document.getElementById("equipment").value;
-    var active = document.getElementById("active");
-
-    if (active.checked == true) {
-        document.getElementById("HUD").innerHTML = selection + " active ";
-        console.log("Weapon Active");
-    } else {
-        document.getElementById("HUD").innerHTML = selection + " selected ";
-        console.log("Weapon Selected");
-    }
-}
 
 // Array of Weapon Options
 var options = [{
@@ -40,16 +50,13 @@ var options = [{
         "selected": true
     },
     {
-        "text": "Fireball",
-        "value": "Fireball"
+        "text": "Sword"
     },
     {
-        "text": "Sword",
-        "value": "Longsword"
+        "text": "Fireball"
     },
     {
-        "text": "Crossbow",
-        "value": "Crossbow"
+        "text": "Crossbow"
     }
 ];
 
@@ -64,84 +71,104 @@ for (var i = 0; i < options.length; i++) {
 // Default GamerInput is set to None
 var gamerInput = new GamerInput("None"); //No Input
 
-var playerImage = new Image();
-playerImage.src = "./img/poke.png";
+//Player
+var sprite = new GameObject("Sprite", "./img/poke.png", 400);
 
-var enemyImage = new Image();
-playerImage.src = "./img/enemy.png";
-
-
-// Default Player
-var sprite = new GameObject("Sprite", playerImage, 100);
-
-var sprite2 = new GameObject("Sprite2", enemyImage, 100);
+//Enemy
+var sprite2 = new GameObject("NPC", "./img/enemy.png", 400);
 
 // Gameobjects is a collection of the Actors within the game
 var gameobjects = [sprite, sprite2];
 
-// Draw a HealthBar on Canvas, can be used to indicate players health
+// Draw a HealthBar on Canvas, indicates players health
 function drawHealthbar() {
-    console.log("drawHealthbar");
+    //console.log("drawHealthbar");
     var width = 100;
     var height = 20;
-    var max = 100;
-    var val = 10;
 
     // Draw the background
     context.fillStyle = "#000000";
-    context.fillRect(gameobjects[0].x, gameobjects[0].y, width, height);
+
+    context.fillRect(360, 250, width, height);
 
     // Draw the fill
-    context.fillStyle = "#00FF00";
-    var fillVal = Math.min(Math.max(val / max, 0), 1);
-    context.fillRect(gameobjects[0].x, gameobjects[0].y, fillVal * width, height);
+    context.fillStyle = "#03a56a";
+    var fillVal = gameobjects[0].health;
+    context.fillRect(360, 250, fillVal / 4, height);
 }
 
+// Draw a HealthBar on Canvas, indicates enemy's health
+function enemyHealthbar() {
+    //console.log("enemyHealthbar");
+    var width = 100;
+    var height = 20;
 
-function attackButton() {
-    gamerInput = new GamerInput("Attack");
-    console.log("Attack");
+    // Draw the background
+    context.fillStyle = "#000000";
+    context.fillRect(100, 20, width, height);
+
+    // Draw the fill
+    context.fillStyle = "#03a56a";
+    var fillVal = gameobjects[1].health;
+    context.fillRect(100, 20, fillVal / 4, height);
 }
 
-function buttonNotPressed() {
-    gamerInput = new GamerInput("None");
-    console.log("Action Stopped");
+//bool for the turn based system
+var playerTurn = true;
+
+// Run when attack clicked
+function attack() {
+    if (playerTurn == true) {
+        gamerInput = new GamerInput("Attack");
+
+        var weaponChoice = selectBox.options[selectBox.selectedIndex].text;
+        console.log(weaponChoice);
+
+        if (weaponChoice == "Sword") {
+            gameobjects[1].health = gameobjects[1].health - 25;
+            playerTurn = false;
+        } else if (weaponChoice == "Fireball") {
+            gameobjects[1].health = gameobjects[1].health - 30;
+            playerTurn = false;
+        } else if (weaponChoice == "Crossbow") {
+            gameobjects[1].health = gameobjects[1].health - 15;
+            playerTurn = false;
+        }
+    }
 }
 
-
-
+//bool for the game to end
+var gameEnd = false;
 
 function update() {
     // Iterate through all GameObjects
     // Updating position and gamestate
     // console.log("Update");
-    for (i = 0; i < gameobjects.length; i++) {
 
-        console.log("Current gamer input:" + gamerInput.action);
+    // Check is input is Attack
 
-        if (gamerInput.action === "Attack") {
-            player_health -= 10;
-            console.log(player_health);
-        }
-
+    if (playerTurn == false) {
+        gameobjects[0].health = gameobjects[0].health - (Math.random() * 15 + 15);
+        playerTurn = true;
     }
 
-}
-var player_health = 100;
+    if (gameobjects[0].health <= 0) {
+        gameEnd = true;
+        var winImage = new Image();
+        winImage.src = "./img/winning_screen.jpg";
+        context.drawImage(winImage, 0, 0, 540, 360);
+    } else if (gameobjects[1].health <= 0) {
+        gameEnd = true;
+        var loseImage = new Image();
+        loseImage.src = "./img/losing_screen.png";
+        context.drawImage(loseImage, 0, 0, 540, 360);
+    }
 
+
+}
 
 var x = 0,
     y = 0;
-
-// Initial time set
-var initial = new Date().getTime();
-var current; // current time
-
-// Total Frames
-var frames = 6;
-
-// Current Frame
-var currentFrame = 0;
 
 // Draw GameObjects to Console
 // Modify to Draw to Screen
@@ -150,33 +177,29 @@ function draw() {
     // Iterate through all GameObjects
     // Draw each GameObject
     // console.log("Draw");
-    context.clearRect(0, 0, canvas.width, canvas.height); // clear canvas
-    console.log("Draw  ...");
 
-    current = new Date().getTime(); // update current
-    if (current - initial >= 500) { // check is greater that 500 ms
-        currentFrame = (currentFrame + 1) % frames; // update frame
-        initial = current; // reset initial
+    if (gameEnd == false) {
+        // Draw image
+        var player = new Image();
+        player.src = gameobjects[0].img;
+        context.drawImage(player, x, y, 200, 200, gameobjects[0].x = 80, gameobjects[0].y = 250, 256, 256);
+        drawHealthbar();
+
+        var npc = new Image();
+        npc.src = gameobjects[1].img;
+        context.drawImage(npc, x, y, 81, 113, gameobjects[1].x = 340, gameobjects[1].y = 20, 81, 113);
+        enemyHealthbar();
     }
-
-    // Draw image
-    context.drawImage(sprite.img, 0, 0, 72, 77, sprite.x, sprite.y, 72, 77);
-    context.drawImage(sprite.img, sprite.x, sprite.y, 912, 512);
-    drawHealthbar();
-
-    context.drawImage(sprite2.img, x, y, 81, 133, sprite2.x, sprite2.y, 81, 133);
-
-    //draw players health
-    context.font = "50px Verdana";
-    context.fillStyle = "black";
-    context.fillText("Enemy Health" + " " + player_health, 255, 120);
-    console.log(player_health);
-
 }
+
+// Initial time set
+var initial = new Date().getTime();
+var current; // current time
 
 function gameloop() {
     update();
     draw();
+    window.requestAnimationFrame(gameloop);
 }
 
 // Handle Active Browser Tag Animation
